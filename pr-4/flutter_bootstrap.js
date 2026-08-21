388e30d096e4ef8a41bf7907521a6043d3e2451a
allapcallapc <allapcallapc@users.noreply.github.com>
2026-08-21 02:31:25 +0000

    deploy: preview for PR #4 59aef230eb775779c6c6d84a50a3bd7c92f4b4d3

--- pr-4/flutter_bootstrap.js
+++ pr-4/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "874606535" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1030615413" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
